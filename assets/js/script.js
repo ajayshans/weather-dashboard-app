b32e93eb8096a9745057dbab2cfc972fa6212a95
Ajay S <ajaycshan@gmail.com>
2023-10-14 22:31:03 +1100

    Search variables JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,11 @@ var apiKey = '36d68183b792de93fe129695d1ec1458';
 
 /* ---------------------- Initialise Variables ---------------------- */
 
-
-
+// Search Section Variables
+var cityInptEl = document.getElementById('city-input');
+var searchBtnEl = document.getElementById('search-button');
+var searchHistEl = document.getElementById('search-history');
+var clearBtnEl = document.getElementById('clear-button');
 
 
 
