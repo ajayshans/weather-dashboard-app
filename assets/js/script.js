9978c811da2d1c0ff875c9fba6f7aca7d312f60e
Ajay S <ajaycshan@gmail.com>
2023-10-15 00:12:32 +1100

    JS Assign city to var + wip lonlat function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,52 @@
 /* ---------------------- Open Weather API Key ---------------------- */
-var apiKey = '36d68183b792de93fe129695d1ec1458';
+const apiKey = '36d68183b792de93fe129695d1ec1458';
 
 /* ---------------------- Initialise Variables ---------------------- */
 
 // Search Section Variables
-var cityInptEl = document.getElementById('city-input');
+var cityInputEl = document.getElementById('city-input');
 var searchBtnEl = document.getElementById('search-button');
 var searchHistEl = document.getElementById('search-history');
 var clearBtnEl = document.getElementById('clear-button');
 
+// Chosen City Variables
+var chosenCityEl = document.getElementById('chosen-city');
+var chosenCountryEl = document.getElementById('chosen-country');
 
 
 /* ---------------------- User Input Handling ---------------------- */
+
+// Assign user input to chosenCity variable
+var assignCity = event => {
+    event.preventDefault();
+    var chosenCity = cityInputEl.value
+    console.log(chosenCity);
+
+    // Alert user if user input is "falsy" (i.e. null, undefined, false, 0, etc)
+    if (!chosenCity) {
+        alert('Please enter a city!');
+        return;
+    }
+
+    obtainLonLatValues(chosenCity);
+}
+
+searchBtnEl.addEventListener('click', assignCity);
+
+// Obtain longitude and langitude of chosenCity
+var obtainLonLatValues = chosenCity => {
+    // Open Weather API URL for geocoding
+    var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + chosenCity + '&limit=1&appid=' + apiKey;
+    fetch(apiUrl)
+    .then(response => {
+        if (response.ok) {
+            response.json()
+            .then(data => {
+                console.log(data);
+            })
+        }
+    })
+}
 
 
 /* ---------------------- Today's Weather Handling ---------------------- */
